b8955b7fd8358f6407953e34cc1abbaabdd1c236
Romulus Ruja <romulus-emanuel.ruja@tutanota.com>
2023-05-21 15:17:18 +0300

    refactor and add new tests for OnionBuilder

--- Message.Tests/OnionBuilderTests.cs
+++ Message.Tests/OnionBuilderTests.cs
@@ -1,37 +1,159 @@
+using Message.Tests.TestData;
 using Crypto.DataProviders;
 using Xunit;
 
 namespace Message.Tests;
 
 public class OnionBuilderTests
-{    
-    [Fact]
-    public void OnionBuilder_ShouldBuild()
+{
+    [Theory]
+    [ClassData(typeof(OnionBuilderTestData))]
+    public void OnionBuilder_ShouldBuild(
+        byte[] content,
+        byte[] nextAddress,
+        params object[] _)
     {
         // Arrange
-        byte[] content = {1, 2, 3, 4, 5, 6, 7, 8};
-        byte[] next = {1, 2, 3, 4, 5, 6, 7, 8};
+        using var context = new AddressContext(nextAddress.Length);
 
         // Act
-        var onion = OnionBuilder.Create().SetMessageContent(content).SetNextAddress(next).Seal(PKey.PublicKey).Build();
+        Action action = () => OnionBuilder
+            .Create()
+            .SetMessageContent(content)
+            .SetNextAddress(nextAddress)
+            .Seal(PKey.PublicKey)
+            .Build();
 
-        Assert.Equal(302, onion.Content.Length);
-        Assert.Equal(1, onion.Content[0]);
-        Assert.Equal(44, onion.Content[1]);
+        // Assert
+        var exception = Record.Exception(action);
+
+        Assert.Null(exception);
     }
 
-    [Fact]
-    public void OnionBuilder_ShouldProduceDifferentContent()
+    [Theory]
+    [ClassData(typeof(OnionBuilderTestData))]
+    public void OnionBuilder_ShouldProduceCorrectCiphertext(
+        byte[] content,
+        byte[] nextAddress,
+        byte[] expectedEncodedSize,
+        ushort expectedTotalSize)
     {
         // Arrange
-        byte[] content = {1, 2, 3, 4, 5, 6, 7, 8};
-        byte[] next = {1, 2, 3, 4, 5, 6, 7, 8};
+        using var context = new AddressContext(nextAddress.Length);
 
         // Act
-        var onion1 = OnionBuilder.Create().SetMessageContent(content).SetNextAddress(next).Seal(PKey.PublicKey).Build();
-        var onion2 = OnionBuilder.Create().SetMessageContent(content).SetNextAddress(next).Seal(PKey.PublicKey).Build();
+        var onion = OnionBuilder
+            .Create()
+            .SetMessageContent(content)
+            .SetNextAddress(nextAddress)
+            .Seal(PKey.PublicKey)
+            .Build();
+
+        // Assert
+        Assert.Equal(expectedEncodedSize, new byte[] { onion.Content[0], onion.Content[1] });
+        Assert.Equal(expectedTotalSize, onion.Content.Length);
+    }
+
+    [Theory]
+    [ClassData(typeof(OnionBuilderTestData))]
+    public void OnionBuilder_ShouldProduceDifferentCiphertextForTheSameContent(
+        byte[] content,
+        byte[] nextAddress,
+        params object[] _)
+    {
+        // Arrange
+        using var context = new AddressContext(nextAddress.Length);
+
+        // Act
+        var onion1 = OnionBuilder
+            .Create()
+            .SetMessageContent(content)
+            .SetNextAddress(nextAddress)
+            .Seal(PKey.PublicKey)
+            .Build();
+        var onion2 = OnionBuilder
+            .Create()
+            .SetMessageContent(content)
+            .SetNextAddress(nextAddress)
+            .Seal(PKey.PublicKey)
+            .Build();
 
         // Assert
         Assert.NotEqual(onion1.Content, onion2.Content);
+    }
+
+    [Theory]
+    [ClassData(typeof(OnionBuilderTestData))]
+    public void OnionBuilder_ShouldThrowExceptionForExceededContentSize(
+        byte[] content,
+        byte[] nextAddress,
+        params object[] _)
+    {
+        // Arrange
+        using var context = new AddressContext(nextAddress.Length);
+
+        // Act
+        Action action = () => OnionBuilder
+            .Create()
+            .SetMessageContent(OnionBuilderTestData.GenerateBytes(ushort.MaxValue + 1))
+            .SetNextAddress(nextAddress)
+            .Seal(PKey.PublicKey)
+            .Build();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal(
+            $"Message content should not exceed {ushort.MaxValue} bytes.",
+            exception.Message);
+    }
+
+    [Theory]
+    [ClassData(typeof(OnionBuilderTestData))]
+    public void OnionBuilder_ShouldThrowExceptionWhenForWrongAddressSize(
+        byte[] content,
+        byte[] nextAddress,
+        params object[] _)
+    {
+        // Arrange
+        using var context = new AddressContext(nextAddress.Length * 2);
+
+        // Act
+        Action action = () => OnionBuilder
+            .Create()
+            .SetMessageContent(content)
+            .SetNextAddress(nextAddress)
+            .Seal(PKey.PublicKey)
+            .Build();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal(
+            $"Destination address length should be exactly {AddressContext.Current.AddressSize} bytes long.",
+            exception.Message);
+    }
+
+    [Theory]
+    [ClassData(typeof(OnionBuilderTestData))]
+    public void OnionBuilder_ShouldThrowExceptionForInvalidEncryptionKey(
+        byte[] content,
+        byte[] nextAddress,
+        params object[] _)
+    {
+        // Arrange
+        using var context = new AddressContext(nextAddress.Length);
+
+        // Act
+        Action action = () => OnionBuilder
+            .Create()
+            .SetMessageContent(content)
+            .SetNextAddress(nextAddress)
+            .Seal("dGhpcyBpcyBhbiBpbnZhbGlkIHB1YmxpYyBrZXk=")
+            .Build();
+
+        // Assert
+        var exception = Assert.Throws<Exception>(action);
+        Assert.Equal(
+            "Message encryption failed.",
+            exception.Message);
     }
 }
---
+++ Message.Tests/TestData/OnionBuilderTestData.cs
@@ -0,0 +1,22 @@
+namespace Message.Tests.TestData;
+
+using System.Collections;
+
+public class OnionBuilderTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { GenerateBytes(128), GenerateBytes(8), new byte[] { 1, 164 }, 422 };
+        yield return new object[] { GenerateBytes(256), GenerateBytes(16), new byte[] { 2, 44 }, 558 };
+        yield return new object[] { GenerateBytes(512), GenerateBytes(32), new byte[] { 3, 60 }, 830 };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public static byte[] GenerateBytes(int count)
+    {
+        var bytes = new byte[count];
+        new Random().NextBytes(bytes);
+        return bytes;
+    }
+}
