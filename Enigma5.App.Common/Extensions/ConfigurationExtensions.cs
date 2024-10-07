4a764fc01e53219b44dd056c2aeea5ae8b95971a
Romulus-Emanuel Ruja <romulus-emanuel.ruja@tutanota.com>
2024-10-07 16:59:25 +0300

    fix config extension function

--- Enigma5.App.Common/Extensions/ConfigurationExtensions.cs
+++ Enigma5.App.Common/Extensions/ConfigurationExtensions.cs
@@ -20,7 +20,7 @@ public static class ConfigurationExtensions
     => configuration.GetValue<string?>("PublicKeyPath", null);
 
     public static string? GetPassphraseKeyPath(this IConfiguration configuration)
-    => configuration.GetValue<string?>("PublicKeyPath", null);
+    => configuration.GetValue<string?>("Passphrase", null);
 
     public static bool GetRetryConnection(this IConfiguration configuration)
     => configuration.GetValue("RetryConnection", false);
