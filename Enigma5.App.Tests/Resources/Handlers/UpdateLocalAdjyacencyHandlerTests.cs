f220116c5a3634f783c6181847b642928c039148
Romulus-Emanuel Ruja <romulus-emanuel.ruja@tutanota.com>
2024-10-12 15:45:13 +0300

    added license

--- Enigma5.App.Tests/Resources/Handlers/UpdateLocalAdjyacencyHandlerTests.cs
+++ Enigma5.App.Tests/Resources/Handlers/UpdateLocalAdjyacencyHandlerTests.cs
@@ -1,9 +1,29 @@
-﻿using Autofac;
+﻿/*
+    Aenigma - Onion Routing based messaging application
+    Copyright (C) 2024  Romulus-Emanuel Ruja <romulus-emanuel.ruja@tutanota.com>
+
+    This file is part of Aenigma project.
+
+    Aenigma is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Aenigma is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Aenigma.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using Autofac;
 using Enigma5.App.Data;
 using Enigma5.App.Models;
 using Enigma5.App.Resources.Commands;
 using Enigma5.App.Resources.Handlers;
-using Enigma5.App.Security.Contracts;
+using Enigma5.Security.Contracts;
 using Enigma5.Crypto.DataProviders;
 
 namespace Enigma5.App.Tests.Resources.Handlers;
--- Enigma5.App/StartupConfiguration.cs
+++ Enigma5.App/StartupConfiguration.cs
@@ -1,3 +1,23 @@
+/*
+    Aenigma - Onion Routing based messaging application
+    Copyright (C) 2024  Romulus-Emanuel Ruja <romulus-emanuel.ruja@tutanota.com>
+
+    This file is part of Aenigma project.
+
+    Aenigma is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Aenigma is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Aenigma.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 using Enigma5.App.Hubs;
 using Enigma5.App.Hubs.Filters;
 using Enigma5.App.Hubs.Sessions;
@@ -15,12 +35,11 @@ using Enigma5.App.Models;
 using Enigma5.Crypto;
 using System.Text;
 using System.Text.Json;
-using Enigma5.App.Security.Contracts;
+using Enigma5.Security.Contracts;
 using MediatR;
 using Enigma5.App.Resources.Queries;
 using Enigma5.App.Common.Extensions;
 using Enigma5.Security;
-using Enigma5.Security.Contracts;
 using Hangfire;
 
 namespace Enigma5.App;
