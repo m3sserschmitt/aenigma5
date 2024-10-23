596aecdf0232e4d6bd7431bdcc0b371b1a9a5f48
Romulus-Emanuel Ruja <romulus-emanuel.ruja@tutanota.com>
2024-10-23 18:45:11 +0300

    added Api class; refactored StartupConfiguration

--- Enigma5.App/StartupConfiguration.cs
+++ Enigma5.App/StartupConfiguration.cs
@@ -22,7 +22,6 @@ using Enigma5.App.Hubs;
 using Enigma5.App.Hubs.Filters;
 using Enigma5.App.Hubs.Sessions;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
@@ -31,17 +30,10 @@ using Enigma5.App.Extensions;
 using Enigma5.App.Hangfire;
 using Enigma5.App.Data;
 using Enigma5.App.Common.Constants;
-using Enigma5.App.Models;
-using Enigma5.Crypto;
-using System.Text;
-using System.Text.Json;
 using Enigma5.Security.Contracts;
-using MediatR;
-using Enigma5.App.Resources.Queries;
 using Enigma5.App.Common.Extensions;
 using Enigma5.Security;
 using Hangfire;
-using Enigma5.App.Resources.Handlers;
 
 namespace Enigma5.App;
 
@@ -86,59 +78,11 @@ public class StartupConfiguration(IConfiguration configuration)
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapHub<RoutingHub>(Endpoints.OnionRoutingEndpoint);
-
-            endpoints.MapGet(Endpoints.InfoEndpoint, async (IMediator commandRouter) =>
-            {
-                var result = await commandRouter.Send(new GetServerInfoQuery());
-                return result.CreateGetResponse();
-            });
-
-            /* endpoints.MapGet(Endpoints.GraphEndpoint, (NetworkGraph networkGraph) =>
-            {
-                //TODO: refactor to use handler
-                return Results.Ok(networkGraph.Graph);
-            });*/
-
-            endpoints.MapGet(Endpoints.VerticesEndpoint, async (IMediator commandRouter) =>
-            {
-                var result = await commandRouter.Send(new GetVerticesQuery());
-                return result.CreateGetResponse();
-            });
-
-            endpoints.MapPost(Endpoints.ShareEndpoint, async (SharedDataCreate sharedDataCreate, IMediator commandRouter, IConfiguration configuration) =>
-            {
-                if (!sharedDataCreate.Valid)
-                {
-                    return Results.BadRequest();
-                }
-
-                var result = await commandRouter.Send(new CreateSharedDataCommand(sharedDataCreate));
-                return result.CreatePostResponse();
-            });
-
-            endpoints.MapGet(Endpoints.ShareEndpoint, async (string tag, IMediator commandRouter) =>
-            {
-                var sharedData = await commandRouter.Send(new GetSharedDataQuery(tag));
-
-                if (!sharedData.IsSuccessNotNullResultValue())
-                {
-                    return Results.NotFound();
-                }
-
-                var result = await commandRouter.Send(new IncrementSharedDataAccessCountCommand(sharedData.Value!.Tag!));
-
-                if(result.IsSuccessNotNullResultValue() && result.Value!.AccessCount > result.Value.MaxAccessCount)
-                {
-                    await commandRouter.Send(new RemoveSharedDataCommand(sharedData.Value.Tag!));
-                }
-                
-                return sharedData.CreateGetResponse();
-            });
-
-            endpoints.MapGet(Endpoints.VertexEndpoint, async (string address, IMediator commandRouter) => {
-                var result = await commandRouter.Send(new GetVertexQuery(address));
-                return result.CreateGetResponse();
-            });
+            endpoints.MapGet(Endpoints.InfoEndpoint, Api.GetInfo);
+            endpoints.MapPost(Endpoints.ShareEndpoint, Api.PostShare);
+            endpoints.MapGet(Endpoints.ShareEndpoint, Api.GetShare);
+            endpoints.MapGet(Endpoints.VerticesEndpoint, Api.GetVertices);
+            endpoints.MapGet(Endpoints.VertexEndpoint, Api.GetVertex);
         });
 
         serviceProvider.UseAsHangfireActivator();
